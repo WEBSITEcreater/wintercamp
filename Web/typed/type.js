c70f50dd6401a4fb853cf7de189686f8985c86b9
WEBSITEcreater <41396119+WEBSITEcreater@users.noreply.github.com>
2019-01-04 10:38:08 -0800

    lol
    non

---
+++ Web/typed/type.js
@@ -0,0 +1,76 @@
+jQuery(document).ready(function() {
+  var type = new Typed('#type', {
+    //GET OUT OF MY CODE YOU MEANIE!
+    strings: [
+      'Games',
+      'I',
+      'Play!',
+      'REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE'
+
+    ],
+    typeSpeed: 1,
+    backSpeed: 1,
+    backDelay: 1500,
+    loop: true,
+  });
+  var title1 = new Typed('#cardTitle1', {
+    strings: [
+      'CSGOOOOO!!!!!!!!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title2 = new Typed('#cardTitle2', {
+    strings: [
+      'Roblox!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title3 = new Typed('#cardTitle3', {
+    strings: [
+      'Fruit Ninja!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title4 = new Typed('#cardTitle4', {
+    strings: [
+      'Another Cool Picture!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title5 = new Typed('#cardTitle5', {
+    strings: [
+      'Another Cool Picture!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title6 = new Typed('#cardTitle6', {
+    strings: [
+      'Another Cool Picture!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+});
---
+++ p5/studentsY/yFruit.js
@@ -0,0 +1,106 @@
+// Create Fruit Function
+function Fruit(x, y, size, color, bad) {
+
+ this.color = color;
+ this.bad = bad;
+ this.size = size;
+
+ this.position = createVector(x,y);
+
+ this.sliced = false;
+ this.slicedTime = 0;
+
+ this.visible = true;
+
+ this.velocity = createVector(randomXVelocity(x), random(-7, -10));
+ 
+}
+// ===================================================
+// ===================================================
+// Don't worry about the code below =]
+// ===================================================
+// ===================================================
+ 
+//Handles position, speed, visibility and cutting time
+
+Fruit.prototype.update = function() {
+
+  this.position.add(this.velocity); //creates movement
+
+  this.velocity.x *= 0.99; // air resistance
+  this.velocity.y += GRAVITY; // gravity
+
+	this.visible = (this.position.y < height); // update visibility of ball
+
+	if (this.sliced) {
+
+		this.slicedTime++; // update slicing time
+	}
+}
+
+// draw fruit and handle fading
+
+Fruit.prototype.draw = function() {
+
+	var fillColor = this.color;
+  if (this.sliced) {
+
+    if (this.bad) {
+      endGame(); //if bad circle is sliced
+    }
+      var interp = constrain(this.slicedTime, 0, 15) / 15; 
+
+		// assigns background color
+    fillColor = lerpColor(this.color, color(224, 224, 224, 0), interp);
+  }
+
+	/* Detemines stroke of bad fruit  */
+  if (this.bad) {
+
+    stroke(168, 146, 25); //black stroke
+    strokeWeight(1); //stroke weight
+  } else {
+
+    noStroke();
+  }
+
+	// draw good ellipse
+  fill(fillColor);
+  ellipse(this.position.x, this.position.y, this.size);
+};
+
+
+//Returns random fruit (good or bad)
+function randomFruit() {
+
+//randomize the position
+  var x = random(width);
+  var y = height;
+
+  var size = noise(frameCount) * 20 + 20; //Randomize the size
+
+  var bad = (random() > BAD_FRUIT_PROBABILITY);
+
+//Change the color of bad fruits to red
+  var r = (bad) ? 45 : 0;
+  var g = (bad) ? 130 : noise(frameCount * 2) * 255;
+  var b = (bad) ? 66 : noise(frameCount * 3) * 255;
+  var a = 255;
+
+  var col = color(r, g, b, a); // Color
+
+  return new Fruit(x, y, size, col, bad); // returns a fruit
+}
+
+
+//Returns the speed to always point towards the center
+function randomXVelocity(x) {
+
+  if (x > width / 2) {
+
+    return random(-1.5, -0.5);
+  } else {
+
+    return random(0.5, 1.5);
+  }
+}
